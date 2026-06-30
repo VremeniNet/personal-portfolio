1464bbf936aa349ebebc23f5fcd985965d0c27b2
Daniil Zhelanov <zdv376@gmail.com>
2026-06-30 22:35:04 +0300

    feat: add hero photo animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import { FiDownload } from 'react-icons/fi'
 
 // components
 import Social from '@/components/Social'
+import Photo from '@/components/Photo'
 
 export default function Home() {
 	return (
@@ -10,7 +11,7 @@ export default function Home() {
 			<div className='container mx-auto h-full'>
 				<div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
 					{/* Text */}
-					<div className='text-center xl:text-left'>
+					<div className='text-center xl:text-left order-2 xl:order-0'>
 						<span className='text-xl'>Фронтенд Разработчик</span>
 						<h1 className='h1 mb-6'>
 							Всем привет, я
@@ -41,7 +42,9 @@ export default function Home() {
 						</div>
 					</div>
 					{/* Photo */}
-					<div>Фото</div>
+					<div className='order-1 xl:order-0 mb-8 xl:mb-0'>
+						<Photo />
+					</div>
 				</div>
 			</div>
 		</section>
